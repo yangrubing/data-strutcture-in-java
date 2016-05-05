4ec31f8aefafe3695c53f14e38b72c46925e434d
yangrubing <yrb2014@bupt.edu.cn>
2016-05-05 20:54:18 +0800

    initial

---
+++ data-structure/src/main/java/com/bupt/yrb/data_structure/App.java
@@ -0,0 +1,13 @@
+package com.bupt.yrb.data_structure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
